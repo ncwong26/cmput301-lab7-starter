056b2c935e468898737d92c34b9b850f0f16fb9f
ncwong26 <nathancwong26@gmail.com>
2025-10-24 15:37:07 -0600

    super working

--- app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
@@ -2,8 +2,6 @@ package com.example.androiduitesting;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static
-        androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -11,7 +9,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import androidx.test.core.app.ActivityScenario;
+
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -24,7 +22,11 @@ import org.junit.runner.RunWith;
 @LargeTest
 public class ShowActivityTest {
     @Rule
-    public ActivityScenarioRule<ShowActivity> scenario = new
+    public ActivityScenarioRule<MainActivity> scenario = new
+            ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Rule
+    public ActivityScenarioRule<ShowActivity> scenario2 = new
             ActivityScenarioRule<ShowActivity>(ShowActivity.class);
 
     @Test
--- app/src/main/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/main/java/com/example/androiduitesting/MainActivityTest.java
@@ -19,6 +19,7 @@ import androidx.test.filters.LargeTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
